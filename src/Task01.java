288ddedc88b324dbe4e94faf8544f141b1f9ca74
vikvik <ivanvikvik@gmail.com>
2022-09-08 09:46:42 +0300

    Updated Task01

--- src/Task01.java
+++ src/Task01.java
@@ -1,9 +1,5 @@
 public class Task01 {
     public static void main(String[] args) {
         System.out.println("Testing GitHub...");
-        System.out.println("Testing GitHub...");
-        System.out.println("Testing GitHub...");
-        System.out.println("Testing GitHub...");
-        System.out.println("Testing GitHub...");
     }
 }
