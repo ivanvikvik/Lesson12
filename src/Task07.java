752f2da5289aeb55f6524d2206629c3d9391d9cf
vikvik <ivanvikvik@gmail.com>
2022-09-08 11:43:19 +0300

    Added Mood Detector Program

--- src/Task07.java
+++ src/Task07.java
@@ -1,2 +1,26 @@
+// :) - happy
+// :( - bad
+// :| - neutral
+// :() - shouted
+// :\ - nervous
+
+import java.util.Random;
+
 public class Task07 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int mood = random.nextInt(5);
+
+        String msg = ":)";
+
+        switch(mood){
+            case 1: msg = ":("; break;
+            case 2: msg = ":|"; break;
+            case 3: msg = ":()"; break;
+            case 4: msg = ":/"; break;
+        }
+
+        System.out.println(msg);
+    }
 }
