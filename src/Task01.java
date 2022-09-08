75998e0e172fca64c7de8e90f23db59cab2883d7
vikvik <ivanvikvik@gmail.com>
2022-09-08 09:56:24 +0300

    Using static method Math.random()

--- src/Task01.java
+++ src/Task01.java
@@ -1,5 +1,9 @@
 public class Task01 {
     public static void main(String[] args) {
-        System.out.println("Testing GitHub...");
+        System.out.println((int)(Math.random() * 100));
+        System.out.println((int)(Math.random() * 100));
+        System.out.println((int)(Math.random() * 100));
+        System.out.println((int)(Math.random() * 100));
+        System.out.println((int)(Math.random() * 100));
     }
 }
