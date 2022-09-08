f5cc63cb7d5dbf9ad0f74d704b1bf385fcb22646
vikvik <ivanvikvik@gmail.com>
2022-09-08 11:24:34 +0300

    Added Coin Program

---
+++ src/Task06.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int count;
+
+        do {
+            System.out.print("Input count of throwing: ");
+            count = scanner.nextInt();
+        } while(count <= 0);
+
+        // business logic
+        int tails = 0;
+
+        for (int i = 0; i < count; i++) {
+            if (random.nextBoolean()) {
+                tails++;
+            }
+        }
+
+        String msg = String.format("Tails = %d, eagle = %d",
+                tails, count - tails);
+
+        System.out.println(msg);
+    }
+}
